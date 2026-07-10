05306f783048e6e5441a3e566c0659df69b198e0
null-route-dev <null-route-dev@protonmail.com>
2026-07-10 20:48:37 +0300

    feat: update custom User model

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['id', 'email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff']
+    list_filter = ['is_active', 'is_staff', 'is_superuser']
+    search_fields = ['email', 'username', 'first_name', 'last_name']
+    ordering = ['-date_joined']
--- users/models.py
+++ users/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
     class Meta:
         db_table = "users"
