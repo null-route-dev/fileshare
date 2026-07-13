6ab18845dda3394a6b0039350333fc6ec58fcf32
null-route-dev <null-route-dev@protonmail.com>
2026-07-13 14:51:32 +0300

    feat: add File model with admin and migrations

--- files/admin.py
+++ files/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+from .models import File
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "size", "uploaded_at"]
+    list_filter = ["uploaded_at"]
+    search_fields = ["name", "user__username", "user__email"]
+    readonly_fields = ["uploaded_at"]
+    fields = ["user", "name", "size", "s3_key", "md5_hash", "mime_type", "uploaded_at"]
--- files/models.py
+++ files/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class File(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="files"
+    )
+    name = models.CharField(max_length=255)
+    size = models.PositiveIntegerField()
+    s3_key = models.CharField(max_length=255, unique=True)
+    md5_hash = models.CharField(max_length=32, blank=True)
+    mime_type = models.CharField(max_length=100, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "files"
+        ordering = ["-uploaded_at"]
